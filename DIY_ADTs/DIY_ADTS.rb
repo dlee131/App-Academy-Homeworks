59899844eca5acb9611fe8ec23e82f76e9ee693a
Daniel Lee <dlee13195@gmail.com>
2022-11-27 20:11:09 -0500

    Stack class

---
+++ DIY_ADTs/DIY_ADTS.rb
@@ -0,0 +1,41 @@
+class Stack
+    def initialize
+        @array = [] 
+    end
+
+    def array
+        @array
+    end
+
+    def push(el)
+        array.push(el)
+        el
+    end
+
+    def pop
+        array.pop
+    end
+
+    def peek
+        array.last
+    end
+
+end
+
+class Queue
+    def initialize
+
+    end
+
+    def enqueue(el)
+
+    end
+    
+    def dequeue
+
+    end
+
+    def peek
+
+    end
+end
