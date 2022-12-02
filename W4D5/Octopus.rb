8d2cb0e0af5422f701a47d805c08ea661688061c
Daniel Lee <dlee13195@gmail.com>
2022-12-02 01:27:18 -0500

    W4D5

---
+++ W4D5/Octopus.rb
@@ -0,0 +1,12 @@
+def Sluggish_Octopus(fish) #Quadratic = nested loops 2x
+    
+    fish.each_with_index do |fish1, idx1|
+        fish.each_with_index do |fish2, idx2|
+            
+       
+        end
+          
+        end
+      
+      end
+end
